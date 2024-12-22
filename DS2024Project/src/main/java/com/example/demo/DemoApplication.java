f4a8ef7ce1505537816af61bdfe8e386cc08de39
陳建穎 <147127484+mikechen111@users.noreply.github.com>
2024-12-22 20:57:15 +0800

    12/22更新

--- DS2024Project/src/main/java/com/example/demo/DemoApplication.java
+++ DS2024Project/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,18 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        logger.info("DemoApplication started on http://localhost:8080");
+    }
 }
-
 
 //操作手則 請先執行這個檔案，然後打開瀏覽器輸入http://localhost:8080/search.html
